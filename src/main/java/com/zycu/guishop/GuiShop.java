96b8a3c0fa164ec9f2997eb73ad01c75f27903b1
Zycu <zamorawade@gmail.com>
2026-06-22 16:53:31 -0400

    Synchronize enchantments and vanilla catalog at server start

--- src/main/java/com/zycu/guishop/GuiShop.java
+++ src/main/java/com/zycu/guishop/GuiShop.java
@@ -2,6 +2,7 @@ package com.zycu.guishop;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 
 public final class GuiShop implements ModInitializer {
@@ -16,6 +17,12 @@ public final class GuiShop implements ModInitializer {
         PLAYERS = new PlayerDirectory();
 
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> ShopCommands.register(dispatcher));
+
+        ServerLifecycleEvents.SERVER_STARTED.register(server -> {
+            CONFIG.ensureEnchantmentDefaults(server);
+            int added = VanillaCatalog.sync(CONFIG, server);
+            System.out.println("[ClassicGUIShop] Vanilla catalog synchronized. Added " + added + " missing listings.");
+        });
 
         ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
             PLAYERS.remember(handler.player);
