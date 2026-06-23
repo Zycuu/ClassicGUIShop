8adc48f5f32e3308b948412bb2369e7d1f90511f
Zycu <zamorawade@gmail.com>
2026-06-23 19:13:39 -0400

    Fix import preview menu type

--- src/main/java/com/zycu/guishop/ImportPreviewGui.java
+++ src/main/java/com/zycu/guishop/ImportPreviewGui.java
@@ -9,6 +9,7 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ChestMenu;
 import net.minecraft.world.inventory.ContainerInput;
+import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 
